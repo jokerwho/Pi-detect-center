98b7e4247ca603996001c4b951772b7d6df132e7
JoyceWedamson <jokerwho@yeah.net>
2019-04-25 16:00:01 +0800

    Update wx_firegas.py

--- wx_firegas.py
+++ wx_firegas.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 import itchat
 
 """set itchat to login in wechat"""
-itchat.auto_login('''hotReload=True''')
+itchat.auto_login(hotReload=True)
 rooms = itchat.get_chatrooms(update=True)
 rooms = itchat.search_chatrooms(name='itchat_test')
 if rooms is not None:
@@ -17,8 +17,15 @@ else:
 GPIO.setmode(GPIO.BCM)
 R,G,Y = 26,19,13
 FIRE = 23
-SMOKE = 21     
+SMOKE = 21
+FMQ = 4
+def afmq():
+    GPIO.setup(FMQ,GPIO.OUT)
+    GPIO.output(FMQ,GPIO.HIGH)
 
+def bfmq():
+    GPIO.setup(FMQ,GPIO.OUT)
+    GPIO.output(FMQ,GPIO.LOW)
 if __name__ == '__main__':
     #itchat.run()
     c = 4
@@ -29,14 +36,19 @@ if __name__ == '__main__':
             GPIO.setup(R,GPIO.OUT)
             GPIO.setup(G,GPIO.OUT)
             GPIO.setup(Y,GPIO.OUT)
+            #GPIO.setup(FMQ,GPIO.OUT)
             #f = GPIO.input(FIRE)
             #y = GPIO.input(SMOKE)
             if GPIO.input(SMOKE) == GPIO.LOW:
+                #GPIO.output(FMQ,GPIO.HIGH)
+                afmq()
                 GPIO.output(G,GPIO.LOW)
                 GPIO.output(Y,GPIO.HIGH)
                 u = "检测到有害气体！"
                 m = 1
                 if GPIO.input(FIRE) == GPIO.HIGH:
+                    #GPIO.output(FMQ,GPIO.HIGH)
+                    afmq()
                     GPIO.output(R,GPIO.HIGH)
                     u = "检测到有害气体和火焰！"
                     m = 3
@@ -46,11 +58,15 @@ if __name__ == '__main__':
             if GPIO.input(SMOKE) == GPIO.HIGH:
                 GPIO.output(Y,GPIO.LOW)
                 if GPIO.input(FIRE) == GPIO.HIGH:
+                    #GPIO.output(FMQ,GPIO.HIGH)
+                    afmq()
                     GPIO.output(G,GPIO.LOW)
                     GPIO.output(R,GPIO.HIGH)
                     u = "检测到火焰！"
                     m = 2
                 if GPIO.input(FIRE) == GPIO.LOW:
+                    #GPIO.output(FMQ,GPIO.LOW)
+                    bfmq()
                     GPIO.output(R,GPIO.LOW)
                     GPIO.output(G,GPIO.HIGH)
                     u = "无异常~"
